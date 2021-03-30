8c7c64bbaa425b9382606a5f20b3219261717381
Aman KUMAR <57890543+CodeOne45@users.noreply.github.com>
2021-03-30 22:26:20 +0200

    Added basic structure

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 const express  =  require('express');
 const morgan = require('morgan');
 
+import postRoutes from './routes/posts.js';
+
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 // get all http request
 app.use(morgan('tiny'));
+app.use('/posts', postRoutes);
 
 app.listen(PORT, console.log('Server is starting at ${PORT}'));
