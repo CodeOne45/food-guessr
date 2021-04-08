621e55c3fb0c72253bdd459f64f74c666262478a
Aman KUMAR <57890543+CodeOne45@users.noreply.github.com>
2021-04-08 18:30:18 +0200

    Tried one page display

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import { Component } from "react";
+import React from "react";
+
 import "./App.css";
 import World from "./World";
 
-function App() {
-  return (
-    <div className="App">
+class Header extends Component {
+  render() {
+    return (
       <header className="App-header">
         <p>Food-Guessr</p>
         <a
@@ -13,13 +16,53 @@ function App() {
         >
           Learn more
         </a>
+        <button>Play</button>
       </header>
+    );
+  }
+}
 
-      <body>
-        <World />
-      </body>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.inputRef = React.createRef();
+    this.state = {
+      showHideHome: true,
+      showHideWorld: false,
+    };
+    this.hideComponent = this.hideComponent.bind(this);
+  }
+
+  hideComponent(name) {
+    console.log(name);
+    switch (name) {
+      case "showHideHome":
+        this.setState({ showHideHome: this.state.Header });
+        break;
+      case "showHideWorld":
+        this.hideComponent("showHideHome");
+        this.setState({ showHideWorld: !this.state.World });
+        break;
+      default:
+        return null;
+    }
+  }
+
+  render() {
+    const { showHideHome, showHideWorld } = this.state;
+    return (
+      <div>
+        <div className="App">
+          <button onClick={() => this.hideComponent("showHideWorld")}>
+            Start
+          </button>
+          {showHideHome && <Header />}
+          <body>{showHideWorld && <World />}</body>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
