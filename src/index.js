01b457c584a26e69709f44e485d3fca7b225b26f
Xiumin Lin <xiumin.lin@laposte.net>
2021-04-08 16:15:27 +0200

    World map is working

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import World from './World';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+  // document.getElementById('root'),
+  <World />,
+  document.getElementById('globeViz')
 );
 
 // If you want to start measuring performance in your app, pass a function
