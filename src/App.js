634cd5dcc375b2c5f8db80bda8356fd6e228443f
steven-van <64859587+steven-van@users.noreply.github.com>
2021-05-07 21:47:45 +0200

    feat/sections

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-/* eslint-disable react/react-in-jsx-scope */
 import React from 'react';
 // import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 // import Header from 'Components/Header/Header';
 // import World from 'Components/World/World';
 // import PageNotFound from 'Components/Error404/PageNotFound';
+import Content from 'Components/Content/Content';
+import CTA from 'Components/CTA/CTA';
+import Footer from 'Components/Footer/Footer';
+import Hero from 'Components/Hero/Hero';
+import LogoClouds from 'Components/LogoClouds/LogoClouds';
+import Team from 'Components/Team/Team';
 
 function App() {
   return (
     <div>
       <div className="App">
+        <Hero />
+        <Content />
+        <LogoClouds />
+        <CTA />
+        <Team />
+        <Footer />
+
         {/* <Router>
           <Switch>
             <Route path="/" exact component={Header} />
