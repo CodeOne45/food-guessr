3a8458f912cdf1cdaff8e1604d5403dde5dddfb5
steven-van <64859587+steven-van@users.noreply.github.com>
2021-05-28 12:32:50 +0200

    feat: animate burger andl logos on hover

--- src/Components/UI/Logo/Logo.js
+++ src/Components/UI/Logo/Logo.js
@@ -1,13 +1,31 @@
-import { React } from 'react';
+import React, { useEffect } from 'react';
+import { gsap } from 'gsap';
 import PropTypes from 'prop-types';
 
 export default function Logo({ src, alt, href }) {
+  const logo = React.createRef();
+  const logoTween = React.createRef();
+
+  useEffect(() => {
+    logoTween.current = gsap.to(logo.current, {
+      scale: 2,
+      paused: true,
+    });
+  });
+
   return (
     <a
       href={href}
       className="col-span-1 flex justify-center py-8 px-8 bg-gray-50"
     >
-      <img className="max-h-12" src={src} alt={alt} />
+      <img
+        className="max-h-12"
+        src={src}
+        alt={alt}
+        ref={logo}
+        onMouseEnter={() => logoTween.current.play()}
+        onMouseLeave={() => logoTween.current.reverse()}
+      />
     </a>
   );
 }
