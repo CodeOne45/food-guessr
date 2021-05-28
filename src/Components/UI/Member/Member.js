c88d3c4990ea10cbdf02f59e7d9c995a4c0b3463
steven-van <64859587+steven-van@users.noreply.github.com>
2021-05-28 15:08:55 +0200

    feat: hero section and member img animation

--- src/Components/UI/Member/Member.js
+++ src/Components/UI/Member/Member.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { gsap } from 'gsap';
 import PropTypes from 'prop-types';
 
 export default function Member({ name, imageUrl, role }) {
+  const member = React.createRef();
+  const memberTween = React.createRef();
+
+  useEffect(() => {
+    memberTween.current = gsap.to(member.current, {
+      scale: 2,
+      paused: true,
+    });
+  });
+
   return (
     <li key={name}>
       <div className="flex items-center space-x-4 lg:space-x-6">
         <img
           className="w-16 h-16 rounded-full bg-yellow-200 lg:w-20 lg:h-20"
           src={imageUrl}
-          alt=""
+          alt={name}
+          ref={member}
+          onMouseEnter={() => memberTween.current.play()}
+          onMouseLeave={() => memberTween.current.reverse()}
         />
         <div className="font-medium text-lg leading-6 space-y-1">
           <h3>{name}</h3>
