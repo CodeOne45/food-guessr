530e1c1847f41592f8fff88ed5d7353cafeecf07
steven-van <64859587+steven-van@users.noreply.github.com>
2021-06-21 18:45:24 +0200

    fix: burger logo animation

--- src/Components/BurgerLogo/BurgerLogo.js
+++ src/Components/BurgerLogo/BurgerLogo.js
@@ -4,24 +4,19 @@ import hamburger from 'Assets/img/hamburger.svg';
 
 export default function BurgerLogo() {
   const burger = React.createRef();
-  const burgerTween = React.useRef();
 
   useEffect(() => {
-    burgerTween.current = gsap.to(burger.current, {
-      rotation: 360,
-      duration: 1,
-      paused: true,
-    });
+    gsap.fromTo(
+      burger.current,
+      {
+        scale: 0,
+        delay: 0.5,
+      },
+      { scale: 1, delay: 0.2, rotation: 360 }
+    );
   });
 
   return (
-    <img
-      src={hamburger}
-      alt="Hamburger"
-      className="h-10 w-auto"
-      ref={burger}
-      onMouseEnter={() => burgerTween.current.play()}
-      onMouseLeave={() => burgerTween.current.reverse()}
-    />
+    <img src={hamburger} alt="Hamburger" className="h-10 w-auto" ref={burger} />
   );
 }
